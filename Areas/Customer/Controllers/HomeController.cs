61f57bdcf2470d2ed0efe84e095b8211ae3f84be
edward028 <130153883+edward028@users.noreply.github.com>
2023-04-07 18:06:43 +0400

    Add files via upload

---
+++ Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopTest.Web.DataAccess.Repositories;
+using ShopTest.Web.Models;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace ShopTest.Web.Areas.Customer.Controllers
+{
+    [Authorize]
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? productId)
+        {
+            Cart cart = new Cart()
+            {
+                Product = _unitOfWork.Product.GetT(x => x.Id == productId, includeProperties: "Category"),
+                Count = 1,
+                ProductId = (int)productId
+            };
+            return View(cart);
+        }
+
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
